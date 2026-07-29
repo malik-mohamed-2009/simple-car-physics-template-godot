aa9260a17bf75f09da2779f5c426ddbaf689863e
MM2009 <malikmohamed11333@gmail.com>
2026-07-29 12:05:34 +0300

    Add files via upload

--- addons/car_physics_template/scripts/main_controller.gd
+++ addons/car_physics_template/scripts/main_controller.gd
@@ -21,14 +21,13 @@ var gear: int
 signal cam_connect
 
 func _physics_process(delta: float) -> void:
-	engine_force = Input.get_axis("ui_down", "ui_up") * get_forced
+	engine_force = Input.get_axis("ui_up", "ui_down") * get_forced
 	
 	steer_input = Input.get_axis("ui_right", "ui_left")
 	steering = move_toward(steering, steer_input * deg_to_rad(25), 0.04)
 	
 	speed_kmh = linear_velocity.length() * 3.6
 	$EngineSfx.pitch_scale = move_toward($EngineSfx.pitch_scale, (speed_kmh / gear_situation[gear + 1]) * energetic_speed_2 + engine_audio_idle_2, 0.1)
-	
 	
 	if speed_kmh > gear_situation[gear] and gear < gear_situation.size() - 2: gear += 1
 	if speed_kmh < gear_situation[gear] - 1: gear -= 1
